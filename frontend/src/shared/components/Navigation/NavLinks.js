e68142ab791ecacc79f9f7f81e65b9ebab96d697
Aanandgupta <anand.1923cs1140@kiet.edu>
2021-10-20 23:33:52 -0700

    icons added

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -11,29 +11,29 @@ const NavLinks = props => {
     <ul className="nav-links">
       <li>
         <NavLink to="/" exact >
-          ALL USERS
+          ALL USERS  <span className="padding"><i class="fa fa-user" aria-hidden="true"></i></span>
         </NavLink>
       </li>
       {auth.isLoggedIn && (
         <li>
-          <NavLink to={`/${auth.userId}/places`}>MY PLACES<i class="fa fa-globe" aria-hidden="true"></i>
+          <NavLink to={`/${auth.userId}/places`}>MY PLACES  <span className="padding"><i class="fa fa-globe" aria-hidden="true"></i></span>
 </NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li>
-          <NavLink to="/places/new">ADD PLACE<i class="fa fa-plus" aria-hidden="true"></i>
+          <NavLink to="/places/new">ADD PLACE  <span className="padding"><i class="fa fa-plus" aria-hidden="true"></i></span>
 </NavLink>
         </li>
       )}
       {!auth.isLoggedIn && (
         <li>
-          <NavLink to="/auth" >AUTHENTICATE<i class="fa fa-sign-in" aria-hidden="true"></i></NavLink>
+          <NavLink to="/auth" >AUTHENTICATE  <span className="padding"><i class="fa fa-sign-in" aria-hidden="true"></i></span></NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li>
-          <button onClick={auth.logout}>LOGOUT<i class="fa fa-sign-out" aria-hidden="true"></i>
+          <button onClick={auth.logout}>LOGOUT  <span className="padding"><i class="fa fa-sign-out" aria-hidden="true"></i></span>
 </button>
         </li>
       )}
